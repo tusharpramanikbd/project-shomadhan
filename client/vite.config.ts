5080053de630c3bbbcf302c965824820996642fa
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-05-15 14:12:51 +0600

    configured vite pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,48 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  devOptions: {
+    enabled: true,
+    type: 'module',
+  },
+  manifest: {
+    name: 'Project Shomadhan',
+    short_name: 'Shomadhan',
+    description: 'Report and track local civic issues.',
+    theme_color: '#ffffff',
+    background_color: '#ffffff',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    id: '/',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA(pwaOptions)],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

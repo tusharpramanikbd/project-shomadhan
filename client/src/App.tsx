39b1b6ea3b613dea966e039f89494ab75d9582c3
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-05-15 13:12:12 +0600

    updated tailwind and daisyui installation and configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Project Shomadhan</h1>
+      <button className="btn btn-error w-64 rounded-full">Click me</button>
     </div>
   );
 }

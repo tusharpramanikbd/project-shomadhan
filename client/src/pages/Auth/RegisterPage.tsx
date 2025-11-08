be0a5649a812bf9db66a3e2075e6822f767d9418
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-11-08 15:20:39 +0600

    added basic email validation and configured with hook form

--- client/src/pages/Auth/RegisterPage.tsx
+++ client/src/pages/Auth/RegisterPage.tsx
@@ -1,21 +1,52 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z.string().email('Invalid email address'),
+});
+
+type FormFields = z.infer<typeof schema>;
+
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues: { email: '' },
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="card glass w-96 shadow-lg">
-        <div className="card-body">
+        <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
           <fieldset className="fieldset">
             <legend className="fieldset-legend">Email Address</legend>
             <input
               type="text"
               className="input"
               placeholder="example@email.com"
+              {...register('email')}
             />
-            {/* <p className="label">Optional</p> */}
+            {errors.email && (
+              <p className="label text-red-500">{errors.email.message}</p>
+            )}
           </fieldset>
           <div className="mt-6">
-            <button className="btn btn-primary btn-block">Register</button>
+            <button
+              disabled={isSubmitting}
+              className="btn btn-primary btn-block"
+            >
+              Register
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
